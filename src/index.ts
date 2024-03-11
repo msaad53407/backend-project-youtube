6af238587a5274349e59364f4260207a2dae01a0
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-03-11 23:17:31 +0500

    Setting up Project Files - part 1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { User } from "./interfaces/todos/user.model";
 
 const app = express();
 
@@ -8,15 +7,6 @@ const PORT = process.env.PORT || 3001;
 app.get('/',
   (req, res) => res.send('Hello World')
 );
-
-
-// User.create({
-//   id: new Date().toString(),
-//   name: 'msaad',
-//   email: 'msaad78@gmail.com',
-//   isActive: 'ACTIVE'
-// }).then((user) => console.log(user))
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
