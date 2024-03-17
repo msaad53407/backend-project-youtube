a17cb553b78e9ca354c693813a6189eeb0fbd76c
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-03-18 02:16:51 +0500

    Add user and video model

---
+++ src/models/video.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { VideoDocument, VideoModel, VideoSchema } from "../interfaces/mongoose.gen";
+
+const videoSchema: VideoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    duration: {
+        type: Number, // This duration will come from Cloudinary or any other 3rd party provider.
+        required: true,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        immutable: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    videoFile: {
+        type: String,
+        required: true,
+    },
+    //TODO: For Future QOL Additions.
+    // likes: {
+    //     type: Number,
+    //     default: 0,
+    // },
+    // dislikes: {
+    //     type: Number,
+    //     default: 0,
+    // }
+}, { timestamps: true });
+
+//TODO look into this error later.
+// @ts-ignore
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video: VideoModel = mongoose.model<VideoDocument, VideoModel>('Video', videoSchema);
