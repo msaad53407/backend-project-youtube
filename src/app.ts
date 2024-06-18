06c0164d3b1de02f1d35b5bb08270dd5c5bc791d
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-06-18 16:02:00 +0500

    Add routes declarations in main app file

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,25 @@ app.use(cookieParser());
 
 // Importing Routes
 import userRouter from "./routes/user.routes";
+import videoRouter from "./routes/video.routes";
+import commentRouter from "./routes/comment.routes";
+import likeRouter from "./routes/like.routes";
+import tweetRouter from "./routes/tweet.routes";
+import playlistRouter from "./routes/playlist.routes";
+import subscriptionRouter from "./routes/subscription.routes";
+import dashboardRouter from "./routes/dashboard.routes";
+import healthCheck from "./routes/healthcheck.routes";
 
 // Routes Declaration
 //& Good and Industry standard practice to use /api/v(whatever the version) before the actual route prefix.
-app.use("/api/v1/users", userRouter);
 // http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthCheck);
+
