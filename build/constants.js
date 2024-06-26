246f63c05f81ee34cce0805bb582dd7821ff9b6d
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-06-27 01:17:46 +0500

    removed build folder

--- build/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jsonLimit = void 0;
-exports.jsonLimit = '16kb';
--- build/routes/healthcheck.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const healthcheck_controller_1 = require("../controllers/healthcheck.controller");
-const router = (0, express_1.Router)();
-router.route('/').get(healthcheck_controller_1.healthCheck);
-exports.default = router;
