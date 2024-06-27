1ba62357eaaf453cf9f651032c7dbafd3d8f3a20
Muhammad Saad <125290250+msaad53407@users.noreply.github.com>
2024-06-27 15:22:04 +0500

    Add methods field in cors configuration

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   })
 );
 app.use(express.json({ limit: jsonLimit }));
